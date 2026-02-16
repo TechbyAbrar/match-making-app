b81fee821cfae1e2a6ab11bca5dd577cb7deb536
TechByAbrar <engr.abrarfahim@gmail.com>
2026-02-16 12:05:32 +0600

    Subscrioption models created

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+class Subscription(models.Model):
+    PLAN_CHOICES = (
+        ('society', 'Society'),
+        ('premium', 'Premium'),
+        ('elite', 'Elite'),
+    )
 
-# Create your models here.
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="subscriptions"
+    )
+
+    plan = models.CharField(max_length=20, choices=PLAN_CHOICES)
+
+    revenuecat_customer_id = models.CharField(max_length=255)
+    revenuecat_entitlement_id = models.CharField(max_length=255)
+
+    is_active = models.BooleanField(default=True)
+
+    started_at = models.DateTimeField()
+    expires_at = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.plan}"
