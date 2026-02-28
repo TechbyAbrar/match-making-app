fda2ba87f76f151b7e3179a7aa1f5b45e887a316
TechByAbrar <engr.abrarfahim@gmail.com>
2026-02-28 15:30:27 +0600

    DeviceDeactivateView business logic added

--- notification/views.py
+++ notification/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.utils import timezone
 from .serializers import DeviceUpsertSerializer
-
+from .models import Device
 class DeviceUpsertView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -14,4 +14,14 @@ class DeviceUpsertView(APIView):
         device = ser.save()
         device.last_seen_at = timezone.now()
         device.save(update_fields=["last_seen_at"])
+        return Response({"ok": True})
+    
+    
+class DeviceDeactivateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        player_id = request.data.get("onesignal_player_id")
+        if player_id:
+            Device.objects.filter(user=request.user, onesignal_player_id=player_id).update(is_active=False)
         return Response({"ok": True})
